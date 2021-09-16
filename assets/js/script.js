fee3d425fb3280ec5f2ffdd602799c3d4ef5b85a
AnthonyStembreit <stembreit@gmail.com>
2021-09-16 14:41:40 -0500

    changed so that li is a form, when add button is hit pushes all checked values to synonyms, created local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+let synonymGroups;
+if(JSON.parse(localStorage.getItem("synonym-groups"))){
+    synonymGroups = JSON.parse(localStorage.getItem("synonym-groups"))
+}else{
+    synonymGroups = []
+}
 function changeView(section) {
     //hides all section tags
     $("section").removeClass("show").addClass("hide")
@@ -17,17 +23,24 @@ $("#searchNav, #inspirationNav, #savedNav").on("click", function (event) {
 })
 
 function generateRandomWord() {
+    //api call that generates random word
     fetch("https://random-word-api.herokuapp.com/word")
         .then(res => res.json())
         .then(data => {
+            //puts random word into html
             $("#randomWord").text(data[0])
         })
 }
+//listens to the new word button and generates new word when clicked
 $("#newWordBtn").on("click", generateRandomWord)
 
 $("#searchBtn").on("click", function (event) {
     event.preventDefault()
-    let word = //TODO get input value
+    let word = $("#searchWord").val().toLowerCase()
+    if(word === ""){
+        //TODO add error message
+        return;
+    }
     generateSynonyms(word)
 })
 
@@ -38,13 +51,39 @@ function generateSynonyms(word) {
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            $("#currentSearch").text(data.word)
             generateListItems(data.synonyms)
         })
 }
-
+generateListItems([
+    "model",
+    "exemplar",
+    "good example",
+    "case",
+    "instance",
+    "illustration",
+    "representative",
+    "deterrent example",
+    "lesson",
+    "object lesson",
+    "exercise"
+  ])
 function generateListItems(synonymsArr) {
     synonymsArr.map(function (syn) {
-        $("#searchResults").append(`<li>${syn} <button value=${syn}>+</button></li>`)
+        $("#searchResults").append(`<li><label>${syn}</label> <input type=checkbox class="pure-input addSyn" value='${syn}'></input></li>`)
     })
 }
-
+$("#saveBtn").on("click", function(event){
+    synObj = {
+        mainWord: $("#currentSearch").val(),
+        synonyms: []
+    }
+    event.preventDefault();
+  $.each( $(".addSyn"), function(){
+       if(this.checked === true){
+            synObj.synonyms.push(this.value)
+       }
+   })
+   synonymGroups.push(synObj)
+   localStorage.setItem("synonym-groups", JSON.stringify(synonymGroups))
+})
