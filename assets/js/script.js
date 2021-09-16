70de360c369b58a4e9fa22b7a7a4c0d781f3e721
AnthonyStembreit <stembreit@gmail.com>
2021-09-16 10:59:11 -0500

    added api call to generate random word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
 function changeView(section) {
+    //hides all section tags
     $("section").removeClass("show").addClass("hide")
+    //shows chosen section
     section.removeClass("hide").addClass("show")
 }
+//listens to all nav links for a click event
 $("#searchNav, #inspirationNav, #savedNav").on("click", function(event){
     event.preventDefault()
-    var section = $("#" + this.id.split("Nav")[0])
-    changeView(section)
+    //grabs current section depending on which nav link was clicked
+    var section =  this.id.split("Nav")[0]
+    //passes that section to the changeView function
+    changeView($("#" + section))
+    console.log(section)
+    if(section === "inspiration"){
+        randomWordApi()
+    }
 })
+
+function randomWordApi(){
+    fetch("https://random-word-form.herokuapp.com/random/adjective")
+    .then( res => res.json())
+    .then( data => {
+        console.log(data)
+        return data
+    })
+}
+
